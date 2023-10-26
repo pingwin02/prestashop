004f9b5a489d628d1774ddf84ae1b1139a7166bb
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-10-27 00:53:46 +0200

    Add script to upload or download backups, updated readme (#10)
    * Add script with Google API to upload or download backups, change docker-compose.yml, updated readme
    
    ---------
    
    Co-authored-by: Maciej Szefler <49917065+rysiekpol@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Prestashop project for Electronic Business
 
+## LIST OF CONTENTS:
+
+- [TECH-STACK](#tech-stack)
+- [HOW TO UPLOAD/DOWNLOAD WEBSITE BACKUP](#how-to-uploaddownload-website-backup)
+- [HOW TO RUN WEBSITE](#how-to-run-website)
+- [ADMIN PANEL](#admin-panel)
+- [RUN SCRAPER](#run-scraper)
+- [AUTHORS](#authors)
+
 ## TECH-STACK:
 
 1. Prestashop v1.7.8-apache
@@ -8,41 +17,89 @@
 4. Python
 5. Scrapy
 
+## HOW TO UPLOAD/DOWNLOAD WEBSITE BACKUP:
+
+1. Move to the `website` directory.
+
+   ```
+   cd website
+   ```
+
+2. Install virtual environment and packages.
+
+   ```
+   python3 -m venv venv
+   source venv/bin/activate
+   pip install -r requirements.txt
+   ```
+
+3. To upload/download or delete backup use:
+
+   ```
+   python backup.py upload/download/delete
+   ```
+
+   (FOR MACOS) After downloading and extracting data, paste this into your console:
+
+   ```
+   sudo chown -R $(id -u):$(id -g) database
+   ```
+
+4. Script will automatically create archive with backup and upload it to the Google Drive or download it and extract to the `website` directory.
+
 ## HOW TO RUN WEBSITE:
 
 1. Install Docker.
 2. Move to the `website` directory.
 
-   ```sh
+   ```
    cd website
    ```
 
-3. Create and run containers (`src` and `database` folders should appear)
+3. Create and run containers using:
 
-   ```sh
-   docker compose up -d
+   ```
+   docker compose up
    ```
 
 4. Open your web browser and go to http://localhost:8080
 
-To stop and remove containers use:
+5. To stop and remove containers use:
 
-```sh
-docker compose down
-```
+   ```
+   docker compose down
+   ```
+
+## ADMIN PANEL:
+
+1. Open your web browser and go to http://localhost:8080/admin123
+
+2. Login using:
+
+   email:
+
+   ```
+   hermiona@ceo.com
+   ```
+
+   password:
+
+   ```
+   hermiona123!@#
+   ```
 
 ## RUN SCRAPER:
 
 1. Install Python on your machine.
 2. Move to the `scraper` directory.
 
-   ```sh
+   ```
    cd scraper
    ```
 
 3. Install virtual environment and packages.
 
-   ```sh
+   ```
    python3 -m venv venv
    source venv/bin/activate
    pip install -r requirements.txt
@@ -50,7 +107,7 @@ docker compose down
 
 4. Run scraper for categories and products.
 
-   ```sh
+   ```
    scrapy crawl categories
    scrapy crawl products
    ```
---
+++ website/backup.py
@@ -0,0 +1,113 @@
+import os
+import sys
+from datetime import datetime
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
+
+gauth = GoogleAuth()
+scope = ["https://www.googleapis.com/auth/drive"]
+gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    'secrets.json', scope)
+drive = GoogleDrive(gauth)
+
+
+def convert_bytes(byte_size):
+    byte_size = float(byte_size)
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if byte_size < 1024.0:
+            break
+        byte_size /= 1024.0
+    return "{:.2f} {}".format(byte_size, unit)
+
+
+def create_tar_archive():
+    print('Creating tar archive...')
+    os.system(f'sudo tar czf {FILENAME} src database')
+
+
+def upload_to_google_drive():
+
+    create_tar_archive()
+    file = drive.CreateFile({'title': FILENAME})
+    file.SetContentFile(f'{FILENAME}')
+    print('Uploading file to Google Drive...')
+    file.Upload()
+    print(f'Uploaded {FILENAME} to Google Drive.')
+
+
+def list_files_in_google_drive():
+
+    file_list = drive.ListFile().GetList()
+
+    if len(file_list) == 0:
+        print('No files found on Google Drive.')
+        sys.exit(1)
+    file_list.sort(key=lambda x: x['createdDate'], reverse=True)
+
+    for i, file in enumerate(file_list):
+        print(f"[{i}] {file['title']} - {convert_bytes(file['fileSize'])}")
+
+    return file_list
+
+
+def download_from_google_drive():
+
+    file_list = list_files_in_google_drive()
+
+    index = int(
+        input('Enter id of the file to download (default: 0): ') or 0)
+
+    if index < 0 or index >= len(file_list):
+        print('Invalid id.')
+        sys.exit(1)
+
+    file = file_list[index]
+    print('Downloading file from Google Drive...')
+    if os.path.exists(file['title']):
+        os.remove(file['title'])
+    file.GetContentFile(file['title'])
+    print('Deleting src and database directories...')
+    os.system('sudo rm -rf src database')
+    print('Extracting tar archive...')
+    os.system(f'sudo tar xzfp {file["title"]}')
+
+
+def delete_from_google_drive():
+
+    file_list = list_files_in_google_drive()
+
+    index = int(
+        input('Enter id of the file to delete (default: 0): ') or 0)
+
+    if index < 0 or index >= len(file_list):
+        print('Invalid id.')
+        sys.exit(1)
+
+    file = file_list[index]
+    print('Deleting file from Google Drive...')
+    file.Delete()
+    print(f'Deleted {file["title"]} from Google Drive.')
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print('Usage: python script.py <upload|download|delete>')
+        sys.exit(1)
+
+    OPERATION = sys.argv[1]
+    FILENAME = f'backup_{datetime.now().replace(microsecond=0).isoformat().replace(":", "-")}.tar.gz'
+
+    print(
+        f"Disk usage: {convert_bytes(drive.GetAbout()['quotaBytesUsed'])} / " +
+        f"{convert_bytes(drive.GetAbout()['quotaBytesTotal'])}")
+
+    if OPERATION == 'upload':
+        upload_to_google_drive()
+    elif OPERATION == 'download':
+        download_from_google_drive()
+    elif OPERATION == 'delete':
+        delete_from_google_drive()
+    else:
+        print('Invalid operation. Please specify either upload or download.')
