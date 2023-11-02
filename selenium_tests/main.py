5821ea3165726b4662ce8ca9b50cd495586471ad
rimeger <qfarmir@gmail.com>
2023-11-02 22:17:59 +0100

    Fixes

--- selenium_tests/main.py
+++ selenium_tests/main.py
@@ -1,27 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
 DEFAULT_LINK = "http://localhost:8080"
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--ignore-ssl-errors=yes')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_experimental_option("detach", True)
+browser = webdriver.Chrome(options=chrome_options)
 
-def test(drive: WebDriver) -> None:
-    drive.get(DEFAULT_LINK)
-    category = drive.find_element(By.XPATH, '//*[@id="category-3"]/a')
-    category.click()
-    first_subcategory = drive.find_element(By.XPATH, '//*[@id="subcategories"]/ul/li[1]/div[1]/a')
-    first_subcategory.click()
-    laptop = drive.find_element(By.XPATH, '//*[@id="js-product-list"]/div[1]/div[1]/article/div/div[1]/a/img')
-    laptop.click()
-    add_to_cart_btn = drive.find_element(By.XPATH, '//*[@id="add-to-cart-or-refresh"]/div[2]/div/div[2]/button')
-    add_to_cart_btn.click()
+
+def test() -> None:
+    browser.get(DEFAULT_LINK)
+    browser.find_element(By.CSS_SELECTOR, "#category-3 > a").click()
+    browser.find_element(By.CSS_SELECTOR, "#subcategories > ul > li:nth-child(1) > div.subcategory-image > a").click()
+    browser.find_element(By.CSS_SELECTOR, "#js-product-list > div.products.row > div:nth-child(1) > article > div > "
+                                          "div.thumbnail-top > a > img").click()
+    browser.find_element(By.CSS_SELECTOR, "#add-to-cart-or-refresh > div.product-add-to-cart.js-product-add-to-cart > "
+                                          "div > div.add > button").click()
 
 
 if __name__ == "__main__":
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--ignore-ssl-errors=yes')
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_experimental_option("detach", True)
-    browser = webdriver.Chrome(options=chrome_options)
-    test(browser)
+    test()
