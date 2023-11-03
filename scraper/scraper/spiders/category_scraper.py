8f72f143e2e7fa286512e1a3c3306519fff218df
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-11-03 15:04:42 +0100

    Fix category for scraped products (#13)
    * Fix scraper, reduced amount of scraped products to ~1300

--- scraper/scraper/spiders/category_scraper.py
+++ scraper/scraper/spiders/category_scraper.py
@@ -14,6 +14,7 @@ class CategoriesSpider(scrapy.Spider):
 
     def parse(self, response):
         categories = {}
+        all_links = []
 
         main_menu = response.css('ul.menu-tree-new')
         for main_category in main_menu.xpath('./li'):
@@ -23,10 +24,14 @@ class CategoriesSpider(scrapy.Spider):
             for sub_category in main_category.xpath('./ul/li'):
 
                 sub_sub_categories = []
-                for sub_sub_category in sub_category.xpath('./ul/li'):
-                    if 'category' in sub_sub_category.css('a::attr(href)').get():
-                        sub_sub_categories.append(
-                            f"https://www.komputronik.pl{sub_sub_category.css('a::attr(href)').get()}")
+                sub_sub_categories_links = [sub_sub_category.css('a::attr(href)').get(
+                ) for sub_sub_category in sub_category.xpath('./ul/li')][:10]
+                for sub_sub_category in sub_sub_categories_links:
+                    if 'category' in sub_sub_category:
+                        link = f"https://www.komputronik.pl{sub_sub_category}"
+                        if link not in all_links:
+                            sub_sub_categories.append(link)
+                            all_links.append(link)
 
                 if len(sub_sub_categories) == 0:
                     continue
