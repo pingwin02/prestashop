a0b046e883186b63971bd10952ad56f91c58daa6
Maciej Szefler <49917065+rysiekpol@users.noreply.github.com>
2023-12-26 19:47:26 +0100

    Fix typo

--- website/prod/init_script.sh
+++ website/prod/init_script.sh
@@ -8,7 +8,7 @@ MYSQL_DATABASE="BE_186044"
 
 SQL_DUMP_FILE="/db_dumb/db.sql"
 
-echo "Creating Database if not exists..." "$MYSQL_DATABASE" < "$SQL_DUMP_FILE"
+echo "Creating Database if not exists..."
 docker exec -it admin-mysql_db mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 
 echo "Applying SQL dump to initialize the database..."
